c442840182a485928edacd14af60ff89fd24e48e
kee_hoon <djfkfk12345@naver.com>
2023-06-28 19:25:32 +0900

    230628

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -36,21 +36,41 @@ function Nav(props){
       </ol>
     </nav>
 }
+
+function Create(props){
+  return <article>
+    <h2>Create</h2>
+    <form onSubmit={event=>{
+      event.preventDefault();
+      const title = event.target.title.value;
+      const body = event.target.body.value;
+      props.onCreate(title,body);
+    }}>
+      <p><input type="text" name="title" placeholder="title"/></p>
+      <p><textarea name="body" placeholder="body"></textarea></p>
+      <p><input type="submit" value="Create"></input></p>
+    </form>
+  </article>
+}
+
 function App() {
   // const _mode = useState('WELCOME');
   // const mode = _mode[0];
   // const setMode = _mode[1];
   const [mode, setMode] = useState("WELCOME");
   const [id, setId] = useState(null);
-  const topics = [
+  const [nextId, setNextId] = useState(4);
+  const [topics, setTopics] = useState([
     {id:1, title:'html', body:'html is ...'},
     {id:2, title:'css', body:'css is ...'},
     {id:3, title:'javascript', body:'javascript is ...'}
-  ]
+  ]);
   let content = null;
-  if(mode === 'WELCOME'){
+
+  if (mode === 'WELCOME'){
     content = <Article title="Welcome" body="Hello, WEB"></Article>
-  } else if(mode === 'READ'){
+  }
+  else if(mode === 'READ'){
     let title, body = null;
     for (let i=0; i<topics.length; i++){
       if (topics[i].id === id){
@@ -59,7 +79,17 @@ function App() {
       }
     }
     content = <Article title={title} body={body}></Article>
-
+  }
+  else if(mode === 'CREATE'){
+    content = <Create onCreate={(_title, _body)=>{
+      const newTopic = {id:nextId, title:_title, body:_body}
+      const newTopics = [...topics]
+      newTopics.push(newTopic);
+      setTopics(newTopics);
+      setMode('READ');
+      setId(nextId);
+      setNextId(nextId+1);
+    }}></Create>
   }
   return (
     <div>
@@ -71,6 +101,10 @@ function App() {
         setId(_id);
       }}></Nav>
       {content}
+      <a href="/create" onClick={event=>{
+        event.preventDefault();
+        setMode('CREATE');
+      }}>Create</a>
     </div>
   );
 }
